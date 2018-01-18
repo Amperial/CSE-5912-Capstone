7b9bb2473569e8beff98459a34d6bc9a0c581ff5
utomo <utomorezeki@gmail.com>
2018-01-18 13:10:46 -0500

    menu prototype

---
+++ SGoopas/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void Play()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
