2d5b4dc7a36aef8e61bad6990c5d096b3dbdd3d2
AangNaruto <AangNaruto93@gmail.com>
2018-01-18 20:09:22 -0500

    Ball now bounces, re-updated project to 2017.3 after breaking it

---
+++ SGoopas/Assets/PongGame/Scripts/Ball/BallMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartMovement();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void StartMovement()
+    {
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(-100, 0, 0));
+    }
+    void StartRandomMovement()
+    {
+        float angle = Random.value * 360;
+        //TODO: apply this force in the direction of the randomly generated angle
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(-1, 0, 0));
+    }
+}
