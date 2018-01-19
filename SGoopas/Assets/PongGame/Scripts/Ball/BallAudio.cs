9d45615ec7b47ecc254a6b7ffcecda8857d13cda
AangNaruto <AangNaruto93@gmail.com>
2018-01-19 07:55:52 -0500

    Sound effects added to the pong game

---
+++ SGoopas/Assets/PongGame/Scripts/Ball/BallAudio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallAudio : MonoBehaviour {
+
+    void OnCollisionEnter(Collision other)
+    {
+        AudioSource boingSound = this.GetComponent<AudioSource>();
+        boingSound.Play();
+    }
+}
---
+++ SGoopas/Assets/PongGame/Scripts/Goals/GoalAudio.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalAudio : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("On Trigger Enter Goal: " + other.gameObject.name);
+        if (other.gameObject.name.Equals("Ball"))
+        {
+            AudioSource scoreSound = this.GetComponent<AudioSource>();
+            scoreSound.Play();
+        }
+    }
+}
