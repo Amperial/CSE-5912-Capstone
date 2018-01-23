a2d6a99f173d953c6d5e51cc163be8c68d70182c
AangNaruto <AangNaruto93@gmail.com>
2018-01-23 11:06:13 -0500

    Small nitpicks for PaddleBounce.cs
    Console Log removed and ammount changed to amount

--- SGoopas/Assets/PongGame/Scripts/Paddle/PaddleBounce.cs
+++ SGoopas/Assets/PongGame/Scripts/Paddle/PaddleBounce.cs
@@ -3,24 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PaddleBounce : MonoBehaviour {
-    public float speedUpAmmount;
-    public float changeDirectionAmmount;
+    public float speedUpAmount;
+    public float changeDirectionAmount;
 
     void OnTriggerExit(Collider other)
     {
-        print("On Trigger Exit: " + other.gameObject.name);
         if(other.gameObject.name.Equals("Ball"))
         {
             GameObject ball = other.gameObject;
             Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
-            Vector3 changeDirection = new Vector3(0, 0, changeDirectionAmmount * (ball.transform.position.z - transform.position.z));
+            Vector3 changeDirection = new Vector3(0, 0, changeDirectionAmount * (ball.transform.position.z - transform.position.z));
             Vector3 speedUp;
             if (ball.transform.position.x > transform.position.x)
             {
-                speedUp = new Vector3(speedUpAmmount, 0, 0);
+                speedUp = new Vector3(speedUpAmount, 0, 0);
             } else
             {
-                speedUp = new Vector3(-speedUpAmmount, 0, 0);
+                speedUp = new Vector3(-speedUpAmount, 0, 0);
             }
             ballRigidbody.AddForce(changeDirection + speedUp);
         }
