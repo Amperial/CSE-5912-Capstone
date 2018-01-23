429e17d48077d5a93c11c88e616b7fc6ac0e6cd2
AangNaruto <AangNaruto93@gmail.com>
2018-01-23 11:40:32 -0500

    Save and Load added to Ball

---
+++ SGoopas/Assets/PongGame/Scripts/Ball/BallSaveAndLoad.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSaveAndLoad : MonoBehaviour {
+    private Vector3 velocity;
+    private Vector3 position;
+    private Vector3 angularVelocity;
+    private new Rigidbody rigidbody;
+    private BallMove movement;
+
+    void Awake()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+        movement = gameObject.GetComponent<BallMove>();
+    }
+	void Save()
+    {
+        velocity = rigidbody.velocity;
+        angularVelocity = rigidbody.angularVelocity;
+        position = transform.position;
+    }
+
+    void Load()
+    {
+        movement.CancelInvoke();
+        rigidbody.velocity = velocity;
+        rigidbody.angularVelocity = angularVelocity;
+        transform.position = position;
+        if(velocity.magnitude <= 0.0001 && velocity.magnitude >= -0.0001)
+        {
+            movement.Invoke("StartRandomMovement", 1f);
+        }
+    }
+}
