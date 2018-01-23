47e7830e4921de4cb6e46fed28674e354a2193ed
Danny Flax <drflax29@verizon.net>
2018-01-22 21:22:32 -0500

    Backbone of game state

---
+++ SGoopas/Assets/Scripts/GameState/PongState.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PongState : IGameState
+{
+    private const string sceneName = "Pong";
+
+    AsyncOperation IGameState.load()
+    {
+        return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+    }
+
+    void IGameState.onEnter()
+    {
+        
+    }
+
+    void IGameState.onExit()
+    {
+        SceneManager.UnloadSceneAsync(sceneName);
+    }
+
+    void IGameState.onPause()
+    {
+        // No-op for now, will eventually freeze items within pong.
+    }
+
+    void IGameState.onUnpause()
+    {
+        // No-op for now, will eventually unfreeze items within the game.
+    }
+}
