5d323273a3d6103cb15ca432babfbc7df3c73b2f
AangNaruto <AangNaruto93@gmail.com>
2018-01-23 10:59:44 -0500

    Removed the TODO from BallMove.cs

--- SGoopas/Assets/PongGame/Scripts/Ball/BallMove.cs
+++ SGoopas/Assets/PongGame/Scripts/Ball/BallMove.cs
@@ -23,7 +23,6 @@ public class BallMove : MonoBehaviour {
         float angle = Random.value * 60;
         angle = angle - 30;
         angle = (float)(Mathf.PI / 180.0 * angle);
-        //TODO: apply this force in the direction of the randomly generated angle
         this.GetComponent<Rigidbody>().AddForce(Vector3.RotateTowards(new Vector3(-initialSpeed, 0, 0), new Vector3(0,0,initialSpeed), angle, initialSpeed));
     }
 }
