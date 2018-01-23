5398cf4f8343566933cfa4720392888267cc49c5
AangNaruto <AangNaruto93@gmail.com>
2018-01-23 14:42:21 -0500

    Sound effects added to the pong game (#9)
    * Sound effects added to the pong game
    
    * Pong game score and gitignore (#10)
    
    * In the process of setting scores
    
    * Added score, moved trigger collider to paddles from ball
    
    * New git ignore to ignore .vs directory propperly
    
    * Fixed scoring
    
    * Removed Comments from score.cs
    
    * Removed Console log from GoalScore.cs
    
    * Remove console log from GoalAudio.cs
    
    * Restart added after score (#11)
    
    * Restart added after score
    
    * Removed the TODO from BallMove.cs
    
    * Changed location of broadcast message
    
    See danny's comment on this pull request: https://github.com/Amperial/CSE-5912-Capstone/pull/11

---
+++ SGoopas/Assets/PongGame/Scripts/Goals/GoalAudio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalAudio : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("Ball"))
+        {
+            AudioSource scoreSound = this.GetComponent<AudioSource>();
+            scoreSound.Play();
+        }
+    }
+}
---
+++ SGoopas/Assets/PongGame/Scripts/UI/Score.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+    private int count;
+
+    public void IncrementScore()
+    {
+        count++;
+    }
+
+    void Start()
+    {
+        count = 0;
+        Text scoreText = this.GetComponent<Text>();
+        UpdateScore(scoreText, count);
+    }
+
+    void Update()
+    {
+        Text scoreText = this.GetComponent<Text>();
+        UpdateScore(scoreText, count);
+
+    }
+
+    void UpdateScore(Text text, int score)
+    {
+        text.text = "Score: " + score.ToString();
+    }
+}
