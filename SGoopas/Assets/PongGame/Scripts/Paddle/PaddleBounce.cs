e3763731743d02b7edc5b00236471d18a6123f5e
AangNaruto <AangNaruto93@gmail.com>
2018-01-23 14:42:59 -0500

    Pong game better ball and paddle (#8)
    * Better ball movement and paddle interactions
    
    * Reverted my git ignore
    
    * Small nitpicks for PaddleBounce.cs
    
    Console Log removed and ammount changed to amount
    
    * Sound effects added to the pong game (#9)
    
    * Sound effects added to the pong game
    
    * Pong game score and gitignore (#10)
    
    * In the process of setting scores
    
    * Added score, moved trigger collider to paddles from ball
    
    * New git ignore to ignore .vs directory propperly
    
    * Fixed scoring
    
    * Removed Comments from score.cs
    
    * Removed Console log from GoalScore.cs
    
    * Remove console log from GoalAudio.cs
    
    * Restart added after score (#11)
    
    * Restart added after score
    
    * Removed the TODO from BallMove.cs
    
    * Changed location of broadcast message
    
    See danny's comment on this pull request: https://github.com/Amperial/CSE-5912-Capstone/pull/11

---
+++ SGoopas/Assets/PongGame/Scripts/Paddle/PaddleBounce.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleBounce : MonoBehaviour {
+    public float speedUpAmount;
+    public float changeDirectionAmount;
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.name.Equals("Ball"))
+        {
+            GameObject ball = other.gameObject;
+            Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
+            Vector3 changeDirection = new Vector3(0, 0, changeDirectionAmount * (ball.transform.position.z - transform.position.z));
+            Vector3 speedUp;
+            if (ball.transform.position.x > transform.position.x)
+            {
+                speedUp = new Vector3(speedUpAmount, 0, 0);
+            } else
+            {
+                speedUp = new Vector3(-speedUpAmount, 0, 0);
+            }
+            ballRigidbody.AddForce(changeDirection + speedUp);
+        }
+    }
+    
+}
