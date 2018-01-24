3bb8667a99bf9b53b48dcfe6dfac1bbd30377b34
Encheat <cheatwood.8@osu.edu>
2018-01-23 23:00:39 -0500

    Weirdest merge ever. Still adding colors and post-processing effects.

---
+++ SGoopas/Assets/PongGame/Scripts/Paddle/AIPaddleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPaddleController : PaddleController {
+
+    public GameObject ball;
+
+    void Start()
+    {
+        if (ball == null)
+        {
+            ball = GameObject.Find("Ball");
+        }
+    }
+
+    void Update () {
+        float paddleZ = paddle.transform.position.z;
+        float ballZ = ball.transform.position.z;
+        MovePaddle(ballZ - paddleZ);
+    }
+}
