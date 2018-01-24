0ec099eab11a589363216c16b2ecd0f23f89ef3d
Austin Payne <payneaustin5@gmail.com>
2018-01-24 11:30:47 -0500

    Improve bounce physics and some editor values

--- SGoopas/Assets/PongGame/Scripts/Paddle/PaddleBounce.cs
+++ SGoopas/Assets/PongGame/Scripts/Paddle/PaddleBounce.cs
@@ -5,25 +5,30 @@ using UnityEngine;
 public class PaddleBounce : MonoBehaviour {
 
     public float speedUpAmount;
-    public float changeDirectionAmount;
+    public float maxBounceVelocity;
 
-    void OnTriggerExit(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name.Equals("Ball"))
+        if (other.gameObject.name.Equals("Ball"))
         {
             GameObject ball = other.gameObject;
             Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
 
-            Vector3 changeDirection = new Vector3(0, 0, changeDirectionAmount * (ball.transform.position.z - transform.position.z));
+            Vector3 velocity = ballRigidbody.velocity;
+            velocity.x *= -1;
+            ballRigidbody.velocity = velocity;
+
+            Vector3 changeDirection = new Vector3(0, 0, maxBounceVelocity * (ball.transform.position.z - transform.position.z) / 3f);
             Vector3 speedUp;
             if (ball.transform.position.x > transform.position.x)
             {
                 speedUp = new Vector3(speedUpAmount, 0, 0);
-            } else
+            }
+            else
             {
                 speedUp = new Vector3(-speedUpAmount, 0, 0);
             }
-            ballRigidbody.AddForce(changeDirection + speedUp);
+            ballRigidbody.AddForce(changeDirection + speedUp, ForceMode.Impulse);
         }
     }
     
