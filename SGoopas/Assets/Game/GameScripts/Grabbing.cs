18d4b6286304ecb50b74ba081b7758723621aa14
Rezeki Utomo <utomorezeki@gmail.com>
2018-01-30 21:01:54 -0500

    working grabbing mechanism. move the cube infront of object and press G to grab and R to release

---
+++ SGoopas/Assets/Game/GameScripts/Grabbing.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabbing : MonoBehaviour {
+
+    private bool grab,trigger;
+    private Collider item;
+	void Start () {
+        grab = false;
+        trigger = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKeyDown(KeyCode.R) && grab)
+        {
+            GameObject parent = transform.parent.gameObject;
+            Destroy(parent.GetComponent<HingeJoint>());
+            item.attachedRigidbody.useGravity = true;
+            grab = false;
+        }
+        if (Input.GetKeyDown(KeyCode.G) && !grab && trigger)
+        {
+            Debug.Log("GRAB INITIATED");
+            GameObject parent = transform.parent.gameObject;
+
+            parent.AddComponent<HingeJoint>();
+
+            HingeJoint joint = parent.GetComponent<HingeJoint>();
+            joint.connectedBody = item.attachedRigidbody;
+            item.attachedRigidbody.useGravity = false;
+
+            JointSpring hingeSpring = joint.spring;
+            hingeSpring.spring = 10;
+            hingeSpring.damper = 3;
+            hingeSpring.targetPosition = 0;
+            joint.spring = hingeSpring;
+            joint.useSpring = true;
+
+            grab = true;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grab)
+        {
+            HingeJoint joint = transform.parent.gameObject.GetComponent<HingeJoint>();
+
+            JointSpring hingeSpring = joint.spring;
+            if(hingeSpring.targetPosition < 60)
+                hingeSpring.targetPosition += 10;
+            joint.spring = hingeSpring;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && grab)
+        {
+            HingeJoint joint = transform.parent.gameObject.GetComponent<HingeJoint>();
+
+            JointSpring hingeSpring = joint.spring;
+            if(hingeSpring.targetPosition > 10)
+                hingeSpring.targetPosition -= 10;
+            joint.spring = hingeSpring;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        trigger = true;
+        item = other;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        trigger = false;
+    }
+
+}
--- SGoopas/Assets/Game/GameScripts/Player3DControl.cs
+++ SGoopas/Assets/Game/GameScripts/Player3DControl.cs
@@ -16,31 +16,46 @@ public class Player3DControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		/* Press R if player is attached to an object (grabbing) to dettach (release)*/
-		if (Input.GetKey (KeyCode.R) && grab) {
-			Destroy (gameObject.GetComponent<FixedJoint> ());
-			grab = false;
-		}
-	}
+        /* Press R if player is attached to an object (grabbing) to dettach (release)*/
+
+        //if (Input.GetKey(KeyCode.R) && grab)
+        //{
+        //    Destroy(gameObject.GetComponent<FixedJoint>());
+        //    grab = false;
+        //}
+    }
 
 	// FixedUpdate for physics update
 	void FixedUpdate () {
+        /*
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        rb.AddForce(new Vector3(moveHorizontal * speed * 20f, 0f, moveVertical * speed * 20f));
+        rb.AddForce(new Vector3(0f, 0f, moveVertical * speed * 5f));
+        */
+        if(Input.GetKey(KeyCode.W))
+            rb.AddRelativeForce(new Vector3(0f, 0f, speed * 5f));
+        else if(Input.GetKey(KeyCode.S))
+            rb.AddRelativeForce(new Vector3(0f, 0f, speed * -5f));
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(new Vector3(0, 1f, 0));
+        else if (Input.GetKey(KeyCode.A))
+            transform.Rotate(new Vector3(0, -1f, 0));
 
         if (Input.GetKeyDown(KeyCode.Space))
             rb.AddForce(new Vector3(0f, speed * 100f, 0f));
     }
 
-	// Needs to be better implemented since right now one has to be pressing G while moving into an object to register grabbing
-	// Instead of being able to grab while near it as well
-	void OnCollisionEnter (Collision collision) {
-		if (Input.GetKey (KeyCode.G)) {
-			gameObject.AddComponent<FixedJoint> ();
-			gameObject.GetComponent<FixedJoint> ().connectedBody = collision.rigidbody;
-			grab = true;
-		}
-	}
+    // Needs to be better implemented since right now one has to be pressing G while moving into an object to register grabbing
+    // Instead of being able to grab while near it as well
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    if (Input.GetKey(KeyCode.G))
+    //    {
+    //        gameObject.AddComponent<FixedJoint>();
+    //        gameObject.GetComponent<FixedJoint>().connectedBody = collision.rigidbody;
+    //        grab = true;
+    //    }
+    //}
 }
