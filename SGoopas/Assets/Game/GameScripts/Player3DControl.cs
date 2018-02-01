6c0e8460c9d3c5ca06bc86a0c232c3d58c52d471
zerox1990 <kevin.impaler@gmail.com>
2018-02-01 13:11:49 -0800

    Deleted commented code

--- SGoopas/Assets/Game/GameScripts/Player3DControl.cs
+++ SGoopas/Assets/Game/GameScripts/Player3DControl.cs
@@ -13,26 +13,8 @@ public class Player3DControl : MonoBehaviour {
         rb = transform.GetComponent<Rigidbody>();
 	}
 
-	// Update is called once per frame
-	void Update () {
-
-        /* Press R if player is attached to an object (grabbing) to dettach (release)*/
-
-        //if (Input.GetKey(KeyCode.R) && grab)
-        //{
-        //    Destroy(gameObject.GetComponent<FixedJoint>());
-        //    grab = false;
-        //}
-    }
-
 	// FixedUpdate for physics update
 	void FixedUpdate () {
-        /*
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        rb.AddForce(new Vector3(0f, 0f, moveVertical * speed * 5f));
-        */
         if(Input.GetKey(KeyCode.W))
             rb.AddRelativeForce(new Vector3(0f, 0f, speed * 5f));
         else if(Input.GetKey(KeyCode.S))
@@ -47,15 +29,4 @@ public class Player3DControl : MonoBehaviour {
             rb.AddForce(new Vector3(0f, speed * 100f, 0f));
     }
 
-    // Needs to be better implemented since right now one has to be pressing G while moving into an object to register grabbing
-    // Instead of being able to grab while near it as well
-    //void OnCollisionEnter(Collision collision)
-    //{
-    //    if (Input.GetKey(KeyCode.G))
-    //    {
-    //        gameObject.AddComponent<FixedJoint>();
-    //        gameObject.GetComponent<FixedJoint>().connectedBody = collision.rigidbody;
-    //        grab = true;
-    //    }
-    //}
 }
