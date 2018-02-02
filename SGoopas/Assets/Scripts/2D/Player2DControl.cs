28202e5827a80ca993a0a4499a0be3bfa3e77fd4
Encheat <cheatwood.8@osu.edu>
2018-02-01 21:42:00 -0500

    Removed extraneous code from Player2DControl.cs

--- SGoopas/Assets/Scripts/2D/Player2DControl.cs
+++ SGoopas/Assets/Scripts/2D/Player2DControl.cs
@@ -9,7 +9,7 @@ public class Player2DControl : MonoBehaviour
     //private bool grounded = false;
 
     private Rigidbody2D rb;
-    public float moveForce = 365f;
+    public float moveForce = 20f;
     private float maxSpeed = 5f;
     private float jumpForce = 1000f;
     public Transform groundCheck;
@@ -34,27 +34,10 @@ public class Player2DControl : MonoBehaviour
     // FixedUpdate for physics update
     void FixedUpdate()
     {
-        /*if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
-            rb.AddRelativeForce(new Vector3(0f, speed * 100f, 0f));
-
-        if (Input.GetKey(KeyCode.D))
-            rb.AddForce(new Vector3(speed* 20f, 0f, 0f));
-        else if (Input.GetKey(KeyCode.A))
-            rb.AddForce(new Vector3(speed * -20f, 0f, 0f));
-
-        if (rb.velocity.x > maxSpeed)
-        {
-            rb.velocity.x = rb.velocity.normalized * maxSpeed;
-        }*/
-
         //store the current horizontal input in the float moveHorizontal
         float moveHorizontal = Input.GetAxis("Horizontal");
-
-        // Store the current vertical input in the float moveVertical.
-        //float moveVertical = Input.GetAxis("Vertical");
         
         //Set horizontal movement
-        if (moveHorizontal * rb.velocity.x < maxSpeed)
             rb.AddForce(Vector2.right * moveHorizontal * moveForce);
         //Cap horizontal movement
         if (Mathf.Abs(rb.velocity.x) > maxSpeed)
