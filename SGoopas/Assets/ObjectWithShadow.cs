6b559083106d8ecff0c1580211f3eebf942e0760
Danny Flax <drflax29@verizon.net>
2018-02-02 15:51:47 -0500

    plane assigned from unity editor

--- SGoopas/Assets/ObjectWithShadow.cs
+++ SGoopas/Assets/ObjectWithShadow.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class ObjectWithShadow : MonoBehaviour {
     public Light shadowLight;
+    public GameObject shadowPlane;
 
     // Use this for initialization
     void Start()
     {
-        ShadowPolygonHelper.CreateShadowGameObject(gameObject, shadowLight.gameObject.transform.position, new Plane(new Vector3(0, 0, 1).normalized, new Vector3(0, 0, 0)));
+        ShadowPolygonHelper.CreateShadowGameObject(gameObject, shadowLight.gameObject.transform.position, new Plane(shadowPlane.transform.up.normalized, new Vector3(0, 0, 0)));
     }
 	
 	// Update is called once per frame
 	void Update () {
-        ShadowPolygonHelper.GetPointLightShadow(shadowLight.gameObject.transform.position, gameObject, new Plane(new Vector3(0, 0, 1).normalized, new Vector3(0, 0, 0)));
+        ShadowPolygonHelper.GetPointLightShadow(shadowLight.gameObject.transform.position, gameObject, new Plane(shadowPlane.transform.up.normalized, new Vector3(0, 0, 0)));
 	}
 }
