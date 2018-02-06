b06810e1588c8d8be4c35d9048cda607d3967b4b
AangNaruto <AangNaruto93@gmail.com>
2018-02-06 12:06:38 -0500

    Adds 2D and 3D swapping
    Adds control for swapping between 2D and 3D via a temporary script, and fixes some issues with the swapping code from before

--- SGoopas/Assets/Scripts/ObjectWithShadow.cs
+++ SGoopas/Assets/Scripts/ObjectWithShadow.cs
@@ -8,40 +8,107 @@ public class ObjectWithShadow : MonoBehaviour {
 	private Rigidbody rb;
 	private MeshRenderer meshRenderer;
 	private GameObject shadow;
+
+    private bool objectIsStatic = false;
+
+    private Vector3 linearVelocity;
+    private Vector3 angularVelocity;
+
     // Use this for initialization
     void Start()
     {
-		meshRenderer = gameObject.GetComponent<MeshRenderer> ();
-		rb = gameObject.GetComponent<Rigidbody> ();
-		shadow = ShadowPolygonHelper.CreateShadowGameObject(gameObject, shadowLight.gameObject.transform.position, new Plane(shadowPlane.transform.up.normalized, new Vector3(0, 0, 0)));
+        meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        rb = gameObject.GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            objectIsStatic = true;
+        }
+        if (objectIsStatic) { 
+            shadow = ShadowPolygonHelper.CreateShadowGameObject(gameObject, shadowLight.gameObject.transform.position, new Plane(shadowPlane.transform.up.normalized, new Vector3(0, 0, 0)));
+            shadow.SetActive(false);
+        } else
+        {
+            linearVelocity = new Vector3();
+            angularVelocity = new Vector3();
+        }
 
-		SwitchTo2D ();
 	}
 
 	/*
-		This methods makes the object transparent and kinematic
+		This methods makes the object transparent and calls either the static or dynamic implementation, depending on the object
 	*/
-	private void SwitchTo2D(){
+	public void SwitchTo2D(){
 
-		if (meshRenderer != null) {
-			meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
-		}
+        if (meshRenderer != null)
+        {
+            meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+        }
 
-		if (rb != null) {
-			rb.isKinematic = true;
-		}
+        if (objectIsStatic)
+        {
+            SwitchTo2DStatic();
+        } else
+        {
+            SwitchTo2DDynamic();
+        }
 	}
 
-	/*
-		This methods makes the object visible and non-kinematic.
+    /*
+		This method makes the object kinematic and generates a shadow
 	*/
-	private void SwitchTo3D(){
+    private void SwitchTo2DDynamic()
+    {
+        linearVelocity = rb.velocity;
+        rb.velocity = new Vector3();
+        angularVelocity = rb.angularVelocity;
+        rb.angularVelocity = new Vector3();
+
+        rb.isKinematic = true;
+
+        shadow = ShadowPolygonHelper.CreateShadowGameObject(gameObject, shadowLight.gameObject.transform.position, new Plane(shadowPlane.transform.up.normalized, new Vector3(0, 0, 0)));
+    }
+    /*
+		This makes the object's shadow active in the hiarchy.
+	*/
+    private void SwitchTo2DStatic()
+    {
+        shadow.SetActive(true);
+    }
+
+	/*
+		This method makes the object visible and calls either the static or dynamic implementation, depending on the object
+	*/
+	public void SwitchTo3D(){
 		if (meshRenderer != null) {
 			meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
 		}
 
-		if (rb != null) {
-			rb.isKinematic = false;
-		}
-	}
+        if (objectIsStatic)
+        {
+            SwitchTo3DStatic();
+        }
+        else
+        {
+            SwitchTo3DDynamic();
+        }
+    }
+    /*
+		This method the rigidbody non-kinematic and destroy's the objects shadow
+	*/
+    private void SwitchTo3DDynamic()
+    {
+        rb.isKinematic = false;
+
+        rb.velocity = linearVelocity;
+        rb.angularVelocity = angularVelocity;
+
+        Destroy(shadow);
+    }
+    /*
+		This method deactivates the object's shadow in the hiarchy
+	*/
+    private void SwitchTo3DStatic()
+    {
+        shadow.SetActive(false);
+    }
 }
