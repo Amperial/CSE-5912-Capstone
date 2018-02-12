58fe3319375ef5e35e7c1d0966a3705d02368d73
Rezeki Utomo <utomorezeki@gmail.com>
2018-02-12 16:10:11 -0500

    added initial state machine

---
+++ SGoopas/Assets/Scripts/Game/Controller/IController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface IController
+{
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+
+    void jump();
+    void grab();
+    void release();
+}
---
+++ SGoopas/Assets/Scripts/Game/Controller/MasterController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterController {
+
+    private IController currentCtrl, threeDCtrl, twoDCtrl;
+	public MasterController(GameObject twoDObj, GameObject threeDObj) {
+        threeDCtrl = new ThreeDCtrl(threeDObj);
+        twoDCtrl = new TwoDCtrl(twoDObj);
+        currentCtrl = threeDCtrl;
+	}
+
+    public void grab()
+    {
+        currentCtrl.grab();
+    }
+
+    public void jump()
+    {
+        currentCtrl.jump();
+    }
+
+    public void moveDown()
+    {
+        currentCtrl.moveDown();
+    }
+
+    public void moveLeft()
+    {
+        currentCtrl.moveLeft();
+    }
+
+    public void moveRight()
+    {
+        currentCtrl.moveRight();
+    }
+
+    public void moveUp()
+    {
+        currentCtrl.moveUp();
+    }
+
+    public void release()
+    {
+        currentCtrl.release();
+    }
+
+    public void switchCtrl()
+    {
+        if (currentCtrl is TwoDCtrl)
+            currentCtrl = threeDCtrl;
+        else
+            currentCtrl = twoDCtrl;
+    }
+}
