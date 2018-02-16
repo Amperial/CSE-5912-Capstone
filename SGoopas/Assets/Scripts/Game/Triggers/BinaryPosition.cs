a583f57bff6bf923c829aa8c49b27c55a62eaf09
Austin Payne <amperialdev@gmail.com>
2018-02-16 13:05:40 -0500

    Add basic trigger system (#58)
    * Create initial trigger system
    
    * Clean up bare minimum trigger system for PR
    
    * Accomplish BinaryPosition movement using a coroutine
    
    * Keep reference to coroutine to avoid using StopAllCoroutines

---
+++ SGoopas/Assets/Scripts/Game/Triggers/BinaryPosition.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BinaryPosition : BinaryTriggerable {
+
+    // Vectors to keep track of initial and active positions
+    private Vector3 initialPosition;
+    private Vector3 initialRotation;
+    public Vector3 activePosition;
+    public Vector3 activeRotation;
+
+    // Total time to move between positions
+    public float moveTime = 1f;
+
+    // Reference to coroutine
+    private Coroutine coroutine;
+    // Elapsed time moving between positions used by coroutine
+    private float elapsed = 0f;
+
+    public void Start() {
+        initialPosition = transform.localPosition;
+        initialRotation = transform.localRotation.eulerAngles;
+    }
+
+    IEnumerator StepToPosition() {
+        // Calculate delta position and rotation for movement to position
+        Vector3 deltaPosition = (initialPosition - activePosition) * Time.fixedDeltaTime / moveTime;
+        Vector3 deltaRotation = (initialRotation - activeRotation) * Time.fixedDeltaTime / moveTime;
+        if (IsActive()) {
+            deltaPosition *= -1;
+            deltaRotation *= -1;
+        }
+        // Move transform by delta position and rotation for specified time
+        while (elapsed < moveTime) {
+            transform.position += deltaPosition;
+            transform.Rotate(deltaRotation);
+            elapsed += Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        // Set transform position and rotation to exact final values
+        transform.localPosition = IsActive() ? activePosition : initialPosition;
+        transform.localRotation = Quaternion.Euler(IsActive() ? activeRotation : initialRotation);
+        elapsed = 0f;
+    }
+
+    public override void Trigger() {
+        base.Trigger();
+        
+        // Check if coroutine is still running or stopped for some reason
+        if (elapsed > 0f) {
+            // Make sure coroutine is stopped
+            StopCoroutine(coroutine);
+            // Calculate elapsed time for new coroutine
+            elapsed = (moveTime - elapsed);
+        }
+
+        // Start coroutine to step to position
+        coroutine = StartCoroutine(StepToPosition());
+    }
+
+}
---
+++ SGoopas/Assets/Scripts/Game/Triggers/BinaryTriggerable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class BinaryTriggerable : MonoBehaviour, ITriggerable {
+
+    private bool active = false;
+
+    public bool IsActive() {
+        return active;
+    }
+
+    public virtual void Trigger() {
+        active = !active;
+    }
+
+}
