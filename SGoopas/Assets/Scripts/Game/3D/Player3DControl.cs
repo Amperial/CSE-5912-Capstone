930e2456b9af6a8756f1c107187fb49ae603f4cf
utomo <utomorezeki@gmail.com>
2018-02-21 16:36:17 -0500

    changed as suggested. attempted to make rotation smoother

--- SGoopas/Assets/Scripts/Game/3D/Player3DControl.cs
+++ SGoopas/Assets/Scripts/Game/3D/Player3DControl.cs
@@ -10,6 +10,7 @@ public class Player3DControl : MonoBehaviour {
     private Transform child;
     private Quaternion forward, back, left, right;
     private float deltaT;
+    private Vector3 forwardForce, backForce, rightForce, leftForce;
 
 	void Start () {
         rb = transform.GetComponent<Rigidbody>();
@@ -21,55 +22,59 @@ public class Player3DControl : MonoBehaviour {
         back = Quaternion.LookRotation(Vector3.back, Vector3.up);
         left = Quaternion.LookRotation(Vector3.left, Vector3.up);
         right = Quaternion.LookRotation(Vector3.right, Vector3.up);
+        forwardForce = new Vector3(0f, 0f, 60f);
+        backForce = new Vector3(0f, 0f, -45f);
+        rightForce = new Vector3(50f, 0f, 0f);
+        leftForce = new Vector3(-50f, 0f, 0f);
     }
 
 	// FixedUpdate for physics update
 	void FixedUpdate () {
         if(Input.GetKey(KeyCode.W) && rb.velocity.magnitude < velocity)
         {
-            rb.AddForce(new Vector3(0f, 0f, 60f));
+            rb.AddForce(forwardForce);
             if (child.rotation != forward) 
             {
                 deltaT = 0.5f;
                 if (Quaternion.Angle(child.rotation, forward) < 90f)
                     deltaT = 0.35f;
-                child.localRotation = Quaternion.Slerp(child.rotation, forward, deltaT);
+                child.localRotation = Quaternion.Lerp(child.rotation, forward, deltaT);
             }
             
         }
         else if(Input.GetKey(KeyCode.S) && rb.velocity.magnitude < velocity)
         {
-            rb.AddForce(new Vector3(0f, 0f, -45f));
+            rb.AddForce(backForce);
             if (child.rotation != back) 
             {
                 deltaT = 0.5f;
                 if (Quaternion.Angle(child.rotation, back) < 90f)
                     deltaT = 0.35f;
-                child.localRotation = Quaternion.Slerp(child.rotation, back, deltaT);
+                child.localRotation = Quaternion.Lerp(child.rotation, back, deltaT);
             }         
         }
 
         if (Input.GetKey(KeyCode.D) && rb.velocity.magnitude < velocity)
         {
-            rb.AddForce(new Vector3(50f, 0f, 0f));
+            rb.AddForce(rightForce);
             if (child.rotation != right) 
             {
                 deltaT = 0.5f;
                 if (Quaternion.Angle(child.rotation, right) < 90f)
                     deltaT = 0.35f;
-                child.localRotation = Quaternion.Slerp(child.rotation, right, deltaT);
+                child.localRotation = Quaternion.Lerp(child.rotation, right, deltaT);
             }
         }
            
         else if (Input.GetKey(KeyCode.A) && rb.velocity.magnitude < velocity)
         {
-            rb.AddForce(new Vector3(-50f, 0f, 0f));
+            rb.AddForce(leftForce);
             if (child.rotation != left) 
             {
                 deltaT = 0.5f;
                 if (Quaternion.Angle(child.rotation, left) < 90f)
                     deltaT = 0.35f;
-                child.localRotation = Quaternion.Slerp(child.rotation, left, deltaT);
+                child.localRotation = Quaternion.Lerp(child.rotation, left, deltaT);
             }
         }
             
