3e6853f99b2c95f60efec5a281414ff80776f4bf
zerox1990 <kevin.impaler@gmail.com>
2018-02-21 13:25:52 -0500

    Button Trigger
    Button Trigger

---
+++ SGoopas/Assets/Scripts/Game/Triggers/ButtonTrigger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour {
+
+    //public Collider other;
+    public GameObject target;
+    private ITriggerable triggerable;
+    public KeyCode key;
+    private bool trigger = false;
+
+    void Start()
+    {
+        triggerable = target.GetComponent<ITriggerable>();
+        gameObject.GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(key) && trigger)
+        {
+            triggerable.Trigger();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        trigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        trigger = false;
+    }
+}
