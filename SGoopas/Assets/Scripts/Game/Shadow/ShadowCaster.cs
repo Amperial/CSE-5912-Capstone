b0d909dd9c4e39d34bf9f136b66252ae1e68884b
Danny Flax <drflax29@verizon.net>
2018-02-24 18:22:54 -0500

    simplification of ShadowPolygonHelper code

--- SGoopas/Assets/Scripts/Game/Shadow/ShadowCaster.cs
+++ SGoopas/Assets/Scripts/Game/Shadow/ShadowCaster.cs
@@ -9,19 +9,26 @@ public abstract class ShadowCaster {
 	protected GameObject shadowObject;
     protected GameObject shadow;
 
-	public abstract void CreateShadow();
     public abstract void UpdateShadow();
-
-    public GameObject GetShadow()
-    {
-        return shadow;
-    }
 
     public ShadowCaster(Light shadowLight, GameObject shadowPlane, GameObject shadowObject) {
 		this.shadowLight = shadowLight;
 		this.shadowPlane = shadowPlane;
 		this.shadowObject = shadowObject;
 	}
+
+    public GameObject GetShadow()
+    {
+        return shadow;
+    }
+
+    public virtual void CreateShadow()
+    {
+        if (shadow)
+        {
+            DestroyShadow();
+        }
+    }
 
     public void ShowShadow()
     {
