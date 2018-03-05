83e6c85ed865a9036d0dfcf3960d0cfd319f2422
zerox1990 <kevin.impaler@gmail.com>
2018-03-05 13:22:45 -0500

    Multiple Positions wth Buttons
    MultiPositionsButton has public Vector3[] positions where one inputs the positions for the gameobject. MultiButtonTrigger has public int index that corresponds to a position in MultiPositionButton positions array.

---
+++ SGoopas/Assets/Scripts/Game/Triggers/MultiButtonTrigger.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class MultiButtonTrigger : MonoBehaviour
+{
+
+    // Works in conjunction with MultiPositionsButton
+
+    public KeyCode key;
+    public GameObject target;
+    private ITriggerable triggerable;
+    private bool trigger = false;
+
+    // Set as 3D player collider
+    public Collider collide;
+
+    public int index;
+
+    void Start()
+    {
+        triggerable = target.GetComponent<ITriggerable>();
+    }
+
+    void Update()
+    {
+
+        // Eventually replacce with action button
+        if (Input.GetKeyDown(key) && trigger)
+        {
+            triggerable.Trigger();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.attachedRigidbody == collide.attachedRigidbody)
+        {
+            trigger = true;
+            Component[] script = GameObject.Find("Platforms").GetComponentsInChildren<MultiPositionsButton>();
+
+            foreach (MultiPositionsButton position in script)
+            {
+                MultiPositionsButton.index = index;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.attachedRigidbody == collide.attachedRigidbody)
+        {
+            trigger = false;
+        }
+    }
+}
