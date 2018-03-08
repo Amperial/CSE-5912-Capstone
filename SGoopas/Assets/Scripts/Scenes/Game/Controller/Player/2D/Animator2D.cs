bfac6230c9056063ad3e4c454dca70d08516cc85
Austin Payne <payneaustin5@gmail.com>
2018-03-08 18:07:32 -0500

    Reorganize entire Scripts directory

---
+++ SGoopas/Assets/Scripts/Scenes/Game/Controller/Player/2D/Animator2D.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator2D {
+
+    //Updates the speedX and speedY parameters for the Animator state machine
+    public static void updateXYParam(Animator anim, Rigidbody2D rb)
+    {
+        anim.SetFloat("speedX", System.Math.Abs(rb.velocity.x));
+        anim.SetFloat("speedY", rb.velocity.y);
+    }
+
+    //Updates the grounded parameter for the Animator state machine
+    public static void updateGroundedParam(Animator anim, bool grounded)
+    {
+        anim.SetBool("grounded", grounded);
+    }
+
+}
