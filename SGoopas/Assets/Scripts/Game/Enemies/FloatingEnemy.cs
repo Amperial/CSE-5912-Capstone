2fa9ff6957cf9663f7b1d38cedeb619efeede910
AangNaruto <AangNaruto93@gmail.com>
2018-03-21 01:28:16 -0400

    Enemies Move

---
+++ SGoopas/Assets/Scripts/Game/Enemies/FloatingEnemy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingEnemy : MonoBehaviour {
+    private bool forwards = true;
+    private int point = 0;
+    Vector3[] points;
+
+    public float speed;
+
+    private bool move = false;
+
+	// Use this for initialization
+	void Start () {
+        points = new Vector3[transform.childCount + 1];
+        points[0] = transform.position;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            points[i + 1] = transform.GetChild(i).position;
+        }
+	}
+	
+    void Move()
+    {
+        float maxDist = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, points[point], maxDist);
+
+        if (forwards)
+        {
+            if (Vector3.Distance(transform.position, points[point]) < 0.0001)
+                point++;
+            if (point == points.Length)
+            {
+                forwards = false;
+                point--;
+            }
+
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position, points[point]) < 0.0001)
+                point--;
+            if (point == -1)
+            {
+                forwards = true;
+                point++;
+            }
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        if (move)
+            Move();
+	}
+
+    void SwitchTo3D(Cancellable cancellable)
+    {
+        cancellable.PerformCancellable(() => move = false, () => move = true);
+    }
+    void SwitchTo2D(Cancellable cancellable)
+    {
+        cancellable.PerformCancellable(() => move = true, () => move = false);
+    }
+}
