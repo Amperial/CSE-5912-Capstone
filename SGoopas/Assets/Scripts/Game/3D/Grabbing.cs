cbceeed76cd66f6c57aecd42bb674cb34507f7ae
Danny Flax <drflax29@verizon.net>
2018-03-24 17:19:38 -0400

    better working grabbable shader

--- SGoopas/Assets/Scripts/Game/3D/Grabbing.cs
+++ SGoopas/Assets/Scripts/Game/3D/Grabbing.cs
@@ -1,69 +1,61 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Grabbing : MonoBehaviour {
     List<Collider> availableObjects = new List<Collider>();
 
     public delegate void GrabAvailabilityChanged(List<Collider> availableObjects);
     public static event GrabAvailabilityChanged grabEvent;
-    Shader highlight;
+    Material highlight;
+    Material ogMaterial;
+    GameObject highlightedObject;
+    static string highlightName = "HighlightMaterial"; 
+
     public void Awake()
     {
-        highlight = Shader.Find("Outlined/Silhouetted Diffuse");
+        Shader highlightShader = Shader.Find("Outline/Transparent");
+        Assert.IsNotNull(highlightShader, "Could not load highlight shader");
+        highlight = new Material(highlightShader);
+        highlight.name = highlightName;
     }
     private void OnTriggerEnter(Collider other)
     {
-
-        ObjInteractable script = other.gameObject.GetComponent<ObjInteractable>();
-        InteractTrigger buttonObj = other.gameObject.GetComponent<InteractTrigger>();
-        if (script != null)
+        ObjInteractable objInteractable = other.gameObject.GetComponent<ObjInteractable>();
+        InteractTrigger trigger = other.gameObject.GetComponent<InteractTrigger>();
+        bool objInteractableAvailable = objInteractable != null && objInteractable.IsPlayerAbleToInteract(gameObject);
+        if (objInteractableAvailable || trigger != null)
         {
-            switch (script.objType)
-            {
-                case ObjInteractable.ObjectType.pushPull:
-                    Vector2 objDir = new Vector2(other.gameObject.transform.position.x - gameObject.transform.parent.position.x, other.gameObject.transform.position.z - gameObject.transform.parent.position.z);
-                    if (Vector2.Angle(objDir.normalized, Vector2.up) < 15f || Vector2.Angle(objDir.normalized, Vector2.down) < 15f || Vector2.Angle(objDir.normalized, Vector2.left) < 15f || Vector2.Angle(objDir.normalized, Vector2.right) < 15f)
-                    {
-                        other.gameObject.GetComponent<Renderer>().material.shader = highlight;
-                        availableObjects.Add(other);
-                        grabEvent(availableObjects);
-                    }
-                    break;
-
-                case ObjInteractable.ObjectType.lift:
-                    other.gameObject.GetComponent<Renderer>().material.shader = highlight;
-                    availableObjects.Add(other);
-                    grabEvent(availableObjects);
-                    break;
-
-            }
-
-            //other.gameObject.GetComponent<Renderer>().material.shader = highlight;
-            //availableObjects.Add(other);
-            //grabEvent(availableObjects);
-        }else if(buttonObj != null)
-        {
-            other.gameObject.GetComponent<Renderer>().material.shader = highlight;
+            HighlightObject(other.gameObject);
             availableObjects.Add(other);
             grabEvent(availableObjects);
         }
     }
 
+    void HighlightObject(GameObject highlightObj) 
+    {
+        if (highlightedObject != null)
+        {
+            UnhighlightObject(highlightObj);
+        }
+        ogMaterial = highlightObj.GetComponent<Renderer>().material;
+        Material[] highligtMaterialSet = {ogMaterial, highlight};
+        highlightObj.GetComponent<Renderer>().materials = highligtMaterialSet;
+        highlightedObject = highlightObj;
+    }
+
+    void UnhighlightObject(GameObject unhighlightObj) 
+    {
+        highlightedObject = null;
+        Material[] defaultMaterialSet = ogMaterial != null ? new Material[] { ogMaterial } : new Material[] {};
+        unhighlightObj.GetComponent<Renderer>().materials = defaultMaterialSet;
+    }
+
     void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.GetComponent<ObjInteractable>() != null)
-        {
-            ObjInteractable script = other.gameObject.GetComponent<ObjInteractable>();
-            other.gameObject.GetComponent<Renderer>().material.shader = script.original;
-            if (availableObjects.Contains(other))
-                availableObjects.Remove(other);
-            grabEvent(availableObjects);
-        }else if(other.gameObject.GetComponent<InteractTrigger>() != null)
-        {
-            InteractTrigger buttonObj = other.gameObject.GetComponent<InteractTrigger>();
-            other.gameObject.GetComponent<Renderer>().material.shader = buttonObj.original;
-            if (availableObjects.Contains(other))
-                availableObjects.Remove(other);
+        if (other.gameObject.Equals(highlightedObject)) {
+            UnhighlightObject(other.gameObject);
+            availableObjects.Remove(other);
             grabEvent(availableObjects);
         }
     }
--- SGoopas/Assets/Scripts/Game/3D/ObjectInterract/ObjInteractable.cs
+++ SGoopas/Assets/Scripts/Game/3D/ObjectInterract/ObjInteractable.cs
@@ -6,10 +6,20 @@ public class ObjInteractable : MonoBehaviour {
 
     public enum ObjectType { pushPull, lift};
     public ObjectType objType;
-    public Shader original;
-    public void Awake()
-    {
-        original = gameObject.GetComponent<Renderer>().material.shader;
-    }
 
+    /*
+     * Implementation of interaction restrictions that are more complicated than proximity 
+     * (e.g. for push pull, the player needs to be facing one of the object's faces)
+     */
+    public bool IsPlayerAbleToInteract(GameObject player) {
+        switch (objType)
+        {
+            case ObjectType.pushPull:
+                Vector2 objDir = new Vector2(gameObject.transform.position.x - player.transform.parent.position.x, gameObject.transform.position.z - player.transform.parent.position.z);
+                return Vector2.Angle(objDir.normalized, Vector2.up) < 15f || Vector2.Angle(objDir.normalized, Vector2.down) < 15f || Vector2.Angle(objDir.normalized, Vector2.left) < 15f || Vector2.Angle(objDir.normalized, Vector2.right) < 15f;
+            case ObjectType.lift:
+            default:
+                return true;
+        }
+    }
 }
