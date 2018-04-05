cd32db4ebf54c55943e102c78aa48545f5862e4b
Danny Flax <drflax29@verizon.net>
2018-04-04 23:48:52 -0400

    Fixed death bug when switching levels

--- SGoopas/Assets/Scripts/Game/Controller/MasterPlayerStateMachine.cs
+++ SGoopas/Assets/Scripts/Game/Controller/MasterPlayerStateMachine.cs
@@ -30,12 +30,8 @@ namespace PlayerStates
             state2D = new StationaryRight2D(player2D, this, groundCheck);
             state2D.StoreState();
             currentState = state3D;
+            PlayerDeathHandler.ResetDeathEvent();
             PlayerDeathHandler.PlayerDeathEvent += PlayerDeathOccurred;
-        }
-
-        ~MasterPlayerStateMachine() {
-            // Unsubscribe from death event when this object is destroyed.
-            PlayerDeathHandler.PlayerDeathEvent -= PlayerDeathOccurred;
         }
 
         public void PlayerDeathOccurred() {
