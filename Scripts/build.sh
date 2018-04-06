a8fb76775557d98984934af7ea31c371c6c0eb95
Danny Flax <drflax29@verizon.net>
2018-04-06 01:07:07 -0400

    better build script

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,3 +1,48 @@
 #! /bin/sh
 
-echo "Build Script not complete yet"
+project="SGoopas"
+
+echo "Attempting to build $project for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile "$(pwd)/$project/unity-windows.log" \
+  -projectPath "$(pwd)/$project" \
+  -buildWindowsPlayer "$(pwd)/$project/Build/windows/$project.exe" \
+  -quit
+
+echo "Windows build:"
+cat $(pwd)/$project/unity-windows.log
+
+zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
+
+echo "Attempting to build $project for OS X"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile "$(pwd)/$project/unity-mac.log" \
+  -projectPath "$(pwd)/$project" \
+  -buildOSXUniversalPlayer "$(pwd)/$project/Build/osx/$project.app" \
+  -quit
+
+echo "OSX build:"
+cat $(pwd)/$project/unity-mac.log
+
+zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
+
+echo "Attempting to build $project for Linux"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile "$(pwd)/$project/unity-linux.log" \
+  -projectPath "$(pwd)/$project" \
+  -buildLinuxUniversalPlayer "$(pwd)/$project/Build/linux/$project.exe" \
+  -quit
+
+echo "Linux build:"
+cat $(pwd)/$project/unity-linux.log
+
+zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
