b0cce4a3757317565033d91f7006224ac53abfd5
utomorezeki <utomorezeki@gmail.com>
2018-04-10 14:35:08 -0400

    fixed skybox. added helper sign prefabs

---
+++ SGoopas/Assets/Scripts/Game/Triggers/BinaryHelp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BinaryHelp : BinaryTriggerable
+{
+    public string[] msg;
+    private MasterMonoBehaviour ptr;
+    private float elapsed;
+    private bool isTrig;
+    public void Start()
+    {
+        isTrig = false;
+        elapsed = 0f;
+        ptr = MasterMonoBehaviour.Instance;
+    }
+
+    public override void Trigger()
+    {
+        base.Trigger();
+        if (ptr != null && !isTrig)
+        {
+            ptr.DisplayMessage(msg);
+        }
+    }
+}
