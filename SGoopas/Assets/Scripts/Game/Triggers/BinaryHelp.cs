2d33b4adec1765c0324ebe809d3704f815e8a50b
utomorezeki <utomorezeki@gmail.com>
2018-04-10 21:38:26 -0400

    removed unnecessary variables

--- SGoopas/Assets/Scripts/Game/Triggers/BinaryHelp.cs
+++ SGoopas/Assets/Scripts/Game/Triggers/BinaryHelp.cs
@@ -5,19 +5,16 @@ public class BinaryHelp : BinaryTriggerable
 {
     public string[] msg;
     private MasterMonoBehaviour ptr;
-    private float elapsed;
-    private bool isTrig;
+
     public void Start()
     {
-        isTrig = false;
-        elapsed = 0f;
         ptr = MasterMonoBehaviour.Instance;
     }
 
     public override void Trigger()
     {
         base.Trigger();
-        if (ptr != null && !isTrig)
+        if (ptr != null)
         {
             ptr.DisplayMessage(msg);
         }
