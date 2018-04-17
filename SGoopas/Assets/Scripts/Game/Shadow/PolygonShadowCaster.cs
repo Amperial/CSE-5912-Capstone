7587927980af7c711883f95d5c3ab7f79fa1babf
Austin Payne <payneaustin5@gmail.com>
2018-04-17 06:15:14 -0400

    Add platform, catapult, and domino puzzles to MLLevel

--- SGoopas/Assets/Scripts/Game/Shadow/PolygonShadowCaster.cs
+++ SGoopas/Assets/Scripts/Game/Shadow/PolygonShadowCaster.cs
@@ -9,6 +9,12 @@ public class PolygonShadowCaster : ShadowCaster {
         shadow = new GameObject();
         shadow.AddComponent<PolygonCollider2D>();
         ShadowPolygonHelper.CalculateShadowForGameObject(shadow, shadowObject, shadowLight, new Plane(shadowPlane.transform.up.normalized, new Vector3()));
+        Rigidbody rb = shadowObject.GetComponent<Rigidbody>();
+        if (rb) {
+            Rigidbody2D rb2d = shadow.AddComponent<Rigidbody2D>();
+            rb2d.bodyType = RigidbodyType2D.Static;
+            rb2d.mass = rb.mass;
+        }
     }
 
     public override void UpdateShadow() {
