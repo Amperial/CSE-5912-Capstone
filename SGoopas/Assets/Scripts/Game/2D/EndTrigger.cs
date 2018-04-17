f183e8e06259a2e8ab159072584cc0d64445361f
Encheat <cheatwood.8@osu.edu>
2018-04-17 03:44:04 -0400

    Added initial portal animation and end. Still require lockout of player controls during end trigger

--- SGoopas/Assets/Scripts/Game/2D/EndTrigger.cs
+++ SGoopas/Assets/Scripts/Game/2D/EndTrigger.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class EndTrigger : MonoBehaviour
 {
+    private Animator anim;
+
+    private void Start()
+    {
+       anim = GetComponent<Animator>();
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
+        //StartCoroutine(ExitLevel());
+        anim.SetBool("exit", true);
         MasterStateMachine.Instance.GoToNextLevel();
     }
+
+    //TODO Create a 3 or 4 second delay before ending the level to allow the animation to play.
+    /*
+    IEnumerator ExitLevel()
+    {
+        print("Hey it's the end!");
+        yield return new WaitForSeconds(0);
+    }
+    */
 }
